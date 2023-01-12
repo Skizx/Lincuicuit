c34c2b5c99fff24e038c6fbd96fa1a332faa6e88
Kevin <kevin.le.milanais@hotmail.fr>
2023-01-12 16:47:10 +0100

    Création middleware Update user + route

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -10,12 +10,38 @@ module.exports.getAllUsers = async (req, res) => {
 
 // Affichage d'un seul utilisateur
 module.exports.getOneUser = async (req,res) => {
-    // Utilisation de la méthode findById pour recupérer l'id de l'objet en la comparant à l'id du parametre de requête
+    // Utilisation d'ObjetID pour servant a vérifier si l'id utilisateur est existant dans  la base de données
     if(!ObjetID.isValid(req.params.id))
     return res.status(400).send('ID Inconnu : ' + req.params.id)
 
+    // Utilisation de la méthode findById pour recupérer l'id de l'objet en la comparant à l'id du parametre de requête
     userModels.findById(req.params.id, (err, docs) => {
         if(!err) res.send(docs);
         else console.log('ID inconnu: ' + err)
     }).select('-password')
+}
+
+module.exports.updateUser = async (req, res) => {
+    // Utilisation d'ObjetID pour servant a vérifier si l'id utilisateur est existant dans  la base de données
+    if(!ObjetID.isValid(req.params.id))
+    return res.status(400).send('ID Inconnu : ' + req.params.id)
+
+    // Utilisation de la méthode findByIdAndUpdate permettant de recupérer l'id et mettre à jour la bio
+    try {
+        userModels.findByIdAndUpdate(
+            {_id: req.params.id},
+            {
+                $set: {
+                    bio: req.body.bio
+                }
+            },
+            {new: true, upsert: true, setDefaultsOnInsert: true},
+            (err, docs) => {
+                if(!err) return res.send(docs);
+                if(err) return res.status(500).json({ message: err})
+            }
+        )
+    } catch(err) {
+        return res.status(500).json({ message: err})
+    }
 }
--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -9,8 +9,9 @@ const router = express.Router();
 // Création des routes authentification
 router.post("/register", authController.signUp);
 
-// Création des routes affichages utilisateurs
+// Création des routes affichages/modification/suppréssion utilisateurs
 router.get("/", user.getAllUsers)
-router.get("/:id" , user.getOneUser)
+router.get('/:id' , user.getOneUser)
+router.put('/:id', user.updateUser)
 
 module.exports = router;
