3e8c7cbeaab6bd079fc58572aee64f51565b63be
Kevin <kevin.le.milanais@hotmail.fr>
2023-01-12 11:34:28 +0100

    Cryptage / Hashage du mot de passe utilisateur avant enregistrement

--- backend/models/user-model.js
+++ backend/models/user-model.js
@@ -1,6 +1,7 @@
 // Importation Mongoose, Unique validator
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
+const bcrypt = require('bcrypt'); 
 
 
 // Création d'un schéma d'un modèle de base de données pour les informations utilisateurs
@@ -46,6 +47,15 @@ const userSchema = mongoose.Schema({
 
 // Application du validator au schéma
 userSchema.plugin(uniqueValidator);
+
+// Application du cryptage du mot de passe avant inscription
+userSchema.pre("save", async function(next) {
+    // Utilisation de genSalt permettant de generer une serie de caractère par bcrypt pour le salage de mot de passe
+    const salt = await bcrypt.genSalt();
+    // Utilisation de la méthode hash de bcrypt qui crée un hash crypté des mots de passe
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
 
 // Exportation du schéma User
 const userModels = mongoose.model('user', userSchema);
