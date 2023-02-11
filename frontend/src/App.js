c1582f8e08dec24e972e8cf3fd3be3800d2ead01
Kevin <kevin.le.milanais@hotmail.fr>
2023-02-11 15:49:37 +0100

    Création hook useContext pour l'accéssibilité a l'utilisateur connecter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,47 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
 import { Route, Routes } from "react-router-dom";
+import axios from "axios";
 import Error from "./Pages/Error";
 import Home from "./Pages/Home";
 import Profil from "./Pages/Profil";
 import Trending from "./Pages/Trending";
+import { UserContext } from "./Hooks/UserContext";
 
 
 function App() {
+
+  const [userId, setUserId] = useState(null)
+
+  useEffect(() => {
+    const getToken = async() => {
+
+      await axios({
+        method : "get",
+        url: `${process.env.REACT_APP_CALL_API}jwtid`,
+        withCredentials: true
+      })
+      .then((res) => {
+        console.log(res)
+        setUserId(res.data)
+      })
+      .catch((err) => console.log("No token"))
+    }
+    getToken()
+  }, [userId]);
+
   return (
     <div>
       <header>
       </header>
       <main>
+        <UserContext.Provider value={userId}>
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="*" element={<Error />}/>
           <Route path="/Trending" element={<Trending />}/>
           <Route path="/Profil" element={<Profil />}/>
         </Routes>
+        </UserContext.Provider>
       </main>
       <footer>
       </footer>
--- frontend/src/Pages/Profil.js
+++ frontend/src/Pages/Profil.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import RegistrationForm from '../Components/Registration/RegistrationForm';
+import  { UserContext } from '../Hooks/UserContext'
 
 const Profil = () => {
+
+    const userId = useContext(UserContext)
+
     return (
         <div className='profil-page'>
+            {userId ? (<h1>PROFIL</h1>) : (
             <div className='registration-container'>
             <RegistrationForm loginForm={true} signupForm={false}/>
             </div>
+            )}
         </div>
     );
 };
